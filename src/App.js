6d904875a530bfcab789a28d7c0cb55a422cd8ba
LvL090 <calvod879@gmail.com>
2022-06-14 16:47:45 +0200

    install router

--- src/App.js
+++ src/App.js
@@ -12,22 +12,22 @@ function App() {
   return (
     <div className='bg-gradient-to-b from-gray-900 via-green-900 to-gray-900 h-screen w-full'>
       <div className='flex'>
-        <section className='w-6/12'>
-            <div className='w-full h-full pt-20'>
-              <DropZone />
-            </div >
-            <div className='flex justify-center'>
-              <Button />
-            </div>
-        </section>
-        <section className='w-6/12'>
-            <div className='w-full h-96 flex justify-center place-items-center'>
-              <Title />
-            </div >
-            <div className='flex justify-center'>
-              <GoogleButton />
-            </div>
-        </section>
+          <section className='w-6/12'>
+              <div className='w-full h-full pt-20'>
+                <DropZone />
+              </div >
+              <div className='flex justify-center'>
+                <Button />
+              </div>
+          </section>
+          <section className='w-6/12'>
+              <div className='w-full h-96 flex justify-center place-items-center'>
+                <Title />
+              </div >
+              <div className='flex justify-center'>
+                <GoogleButton />
+              </div>
+          </section>
       </div>
     </div>
 
