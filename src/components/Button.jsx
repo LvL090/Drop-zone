fb19183d539d9ac947336b389e7a2cb7ddb51a8b
LvL090 <calvod879@gmail.com>
2022-06-15 15:57:28 +0200

    add alert error

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,8 +1,20 @@
 import React from 'react';
+import swal from 'sweetalert';
 
-const Button = () => {
+
+function Button () {
+
+const showAlert = () => {
+    swal ({
+        title: "Upps..",
+        text: "Parece que algo falla :(",
+        icon: "error",
+        button: "Aceptar",
+    });
+}
+
     return (
-        <button className=' w-11/12 text-white bg-gradient-to-br from-gray-400 to-green-600 hover:bg-gradient-to-bl 00  font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2'>
+        <button onClick={()=>showAlert()} className=' w-11/12 text-white bg-gradient-to-br from-gray-400 to-green-600 hover:bg-gradient-to-bl 00  font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2'>
             Subir Archivos
         </button>
     );
